e42e29e977ad3bd4f42fb8fc1e5049a945c5a5d0
Ch-Bun <charlotte.bunnenberg@gmx.de>
2022-09-15 16:00:49 +0200

    Solved exercises

--- src/ba.py
+++ src/ba.py
@@ -2,36 +2,63 @@
 
 
 def border_array(x: str) -> list[int]:
-    """
-    Construct the border array for x.
+    #edge case
+    if x == "":
+        return []
+    #built border array
+    ba = [0]*len(x)
+    
+    #otherwise run trough seq
+    for i in range(1, len(x)):
+        b = ba[i-1]
+        #extend border
+        while (b > 0) and (x[i] != x[b]):
+            b = ba[b-1]
+        #if matches
+        if(x[i]==x[b]):
+            ba[i] = b + 1
+        else:
+            ba[i] = 0
 
-    >>> border_array("aaba")
-    [0, 1, 0, 1]
-    >>> border_array("ississippi")
-    [0, 0, 0, 1, 2, 3, 4, 0, 0, 1]
-    >>> border_array("")
-    []
-    """
-    return []  # FIXME
+    return ba
 
 
 def strict_border_array(x: str) -> list[int]:
-    """
-    Construct the strict border array for x.
+    #edge case
+    if x == "":
+        return []
+        
+    ba = border_array(x)
+    #built border array
+    bax = [0]*len(x)
+    
+    #otherwise run trough seq
+    for i in range(len(x)-1):
+        if (ba[i] != 0 and x[ba[i]] != x[i+1]):
+            bax[i] = ba[i]
+        else:
+            bax[i] = 0
+    bax[len(x)-1] = ba[len(x)-1]
+    return bax
 
-    A struct border array is one where the border cannot
-    match on the next character. If b is the length of the
-    longest border for x[:i+1], it means x[:b] == x[i-b:i+1],
-    but for a strict border, it must be the longest border
-    such that x[b] != x[i+1].
+def main():
+    
+    #Test
+    print(border_array("aaba"))
+    #[0, 1, 0, 1]
+    print(border_array("ississippi"))
+    #[0, 0, 0, 1, 2, 3, 4, 0, 0, 1]
+    print(border_array(""))
+    #[]
 
-    >>> strict_border_array("aaba")
-    [0, 1, 0, 1]
-    >>> strict_border_array("aaaba")
-    [0, 0, 2, 0, 1]
-    >>> strict_border_array("ississippi")
-    [0, 0, 0, 0, 0, 0, 4, 0, 0, 1]
-    >>> strict_border_array("")
-    []
-    """
-    return []  # FIXME
+    print(strict_border_array("aaba"))
+    #[0, 1, 0, 1]
+    print(strict_border_array("aaaba"))
+    #[0, 0, 2, 0, 1]
+    print(strict_border_array("ississippi"))
+    #[0, 0, 0, 0, 0, 0, 4, 0, 0, 1]
+    print(strict_border_array(""))
+    #[]
+
+if __name__ == '__main__':
+    main()
